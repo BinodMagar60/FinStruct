18381092e6f72552d0a4b159bb81b473e4496347
unknown <kauchabinod88@gmail.com>
2025-03-06 09:59:43 +0545

    unknown

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './Login'
+import Navbar from './Components/NavBar'
+// import Loading from './Components/Loading'
+
+const App = () => {
+  return (
+    <>
+    {/* <Loading/> */}
+    <Navbar/>
+    <Login/>
+    </>
+  )
+}
+
+export default App
--- Frontend/src/Components/Loading.jsx
+++ Frontend/src/Components/Loading.jsx
@@ -2,17 +2,9 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div class="flex gap-2 absolute z-100 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-    <div class='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-	<div class='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
-	<div class='h-5 w-5 bg-black rounded-full animate-bounce'></div>
-    <div class='h-5 w-5 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
-	
+    <div className='absolute z-10 w-full h-screen bg-transparent'>
+      <div class="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-black absolute z-99 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"></div>
     </div>
-
-
-
-
   )
 }
 
