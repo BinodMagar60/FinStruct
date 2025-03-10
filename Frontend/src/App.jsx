a09c73dcebf13a67b3befd1349b3012faf6c91c1
unknown <kauchabinod88@gmail.com>
2025-03-10 16:50:10 +0545

    login/signup validation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Admin from './Pages/admin/Admin'
+import Login from './Login'
 
 
 const App = () => {
   return (
     <div className='bg-[#efefef] h-screen'>
-    <Admin />
+    {/* <Admin /> */}
+    <Login/>
     </div>
   )
 }
