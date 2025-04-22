4ce77a466cace3796ef7d0a35513712643a748ae
CdySirjan <yoursirjan@gmail.com>
2025-04-22 16:40:28 +0545

    the minor error was fixed of the backend

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,12 +4,19 @@ const User = require("../User/User");
 
 router.post("/add-user", async (req, res) => {
     try {
-        const newUser = new User(req.body);
-        await newUser.save();
-        res.status(201).json({ message: "User added successfully" });
+      console.log("Request body:", req.body); 
+  
+      const newUser = new User(req.body);
+      await newUser.save();
+  
+      res.status(201).json({ message: "User added successfully" });
     } catch (err) {
-        res.status(500).json({ error: "Error adding user" });
+      console.error("Error saving user:", err.message);
+      res.status(500).json({ error: err.message }); 
     }
-});
+  });
+  
 
 module.exports = router;
+
+
